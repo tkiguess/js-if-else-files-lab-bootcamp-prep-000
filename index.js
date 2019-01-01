95e8b7933a8d2523bfa3cfa411aa14cee0f08f87
Tafkir Ahmed <ahmedtafkir@gmail.com>
2019-01-01 09:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+ name)
+}
+
+
+
+if (index.height === 74) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+ height)
+}
